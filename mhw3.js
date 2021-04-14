d29f06646f012918d3012135d4291a500a5ace58
Caggegi <rosariocaggegi99@gmail.com>
2021-04-14 23:05:08 +0200

    prova modifica banner

---
+++ mhw3.js
@@ -0,0 +1,3 @@
+const header = document.querySelector("header");
+
+header.style.backgroundImage("url(https://9to5mac.com/wp-content/uploads/sites/6/2020/10/The-Lake-5-dragged.jpg?quality=82&strip=all&w=1000)");
