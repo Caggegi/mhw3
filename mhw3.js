d08b616d23736912f5fc5d15e0f1a23d8a966eed
Caggegi <rosariocaggegi99@gmail.com>
2021-04-14 23:32:46 +0200

    random header

--- mhw3.js
+++ mhw3.js
@@ -1,3 +1,14 @@
 const header = document.querySelector("header");
 
-header.style.backgroundImage("url(https://9to5mac.com/wp-content/uploads/sites/6/2020/10/The-Lake-5-dragged.jpg?quality=82&strip=all&w=1000)");
+//header.style.backgroundImage = "url(https://9to5mac.com/wp-content/uploads/sites/6/2020/10/The-Lake-5-dragged.jpg?quality=82&strip=all&w=1000)";
+
+fetch("https://picsum.photos/700/300").then(onResponse, onError);
+
+function onResponse(response){
+    console.log(response);
+    header.style.backgroundImage = "url("+response.url+")";
+}
+
+function onError(error){
+    console.log(error);
+}
