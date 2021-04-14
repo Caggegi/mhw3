5d4bf83feb91b81ef9f0aaa0fc07b6343064e6aa
Caggegi <rosariocaggegi99@gmail.com>
2021-04-14 23:59:50 +0200

    fix 3

--- mhw3.js
+++ mhw3.js
@@ -1,6 +1,6 @@
 const header = document.querySelector("header div#background");
 
-//header.style.backgroundImage = "url(https://9to5mac.com/wp-content/uploads/sites/6/2020/10/The-Lake-5-dragged.jpg?quality=82&strip=all&w=1000)";
+
 fetch("https://picsum.photos/2800/700").then(onResponse, onError);
 
 function onResponse(response){
@@ -10,4 +10,5 @@ function onResponse(response){
 
 function onError(error){
     console.log(error);
+    header.style.backgroundImage = "url(img/default.jpg)";
 }
